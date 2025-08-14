5d3a89ede5779e4f0be5379b1d2c464ee056097e
roydeenn-boop <roydeenn@gmail.com>
2025-08-14 15:22:41 +0300

    Update index.js

--- invite-usdt-mono/apps/web/pages/index.js
+++ invite-usdt-mono/apps/web/pages/index.js
@@ -1,7 +1,7 @@
 
 import { useState } from 'react';
 
-const API = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:4000';
+const API = process.env.NEXT_PUBLIC_API_URL || 'https://invite-usdt-api.vercel.app/';
 
 export default function Home(){
   const [token, setToken] = useState('');
